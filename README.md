b6240339f86493a91275e50b85a9173e7b167334
yangzj1992 <yangzj1992@qq.com>
2015-12-24 14:23:00 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # TKL-REVISION
+TKL-REVISION is a responsive design theme for Hexo.
+###Install
+Execute the following command and modify theme in <code>_config.yml</code> to <code>TKL</code>.
+```
+if u like the original version:
+git clone https://github.com/SuperKieran/TKL.git
+
+or come to my modify version:
+git clone https://github.com/yangzj1992/TKL.git
+```
+<!--more-->
+###Update
+Execute the following command to update TKL.
+``` 
+cd themes/TKL
+git pull
+```
+###Config
+####Theme_config.yml
+```
+cover: /img/bg_img.jpg
+logo: /img/logo.png
+top_saying:
+- title: YOU'VE MADE A <span>BRAVE</span> DECISION, WELCOME.
+- content: 每一个不曾起舞的日子都是对生命的辜负。
+bottom_saying: 虽然还没想好写点什么，但是总觉得这里放句话比较和谐。
+  
+excerpt_link: Read More
+  
+rss: /atom.xml
+  
+highlightjs: vs
+
+works:
+- works_name: XXX1
+  works_url: /
+- works_name: XXX2
+  works_url: /
+  
+duoshuo: duoshuo_name
+  
+github: https://github.com/
+twitter: https://twitter.com/
+facebook: https://www.facebook.com/
+google:  https://google.com/
+weibo: http://weibo.com/
+  
+timeline:
+- num: 1
+  word: 2014/06/12-Start xxx
+- num: 2
+  word: 2014/11/29-Start bbb
+- num: 3
+  word: 2015/02/18-Start ddd
+- num: 4
+  word: ...
+  
+links:
+- name: Kieran
+  link: http://go.kieran.top/
+- name: Name
+  link: http://domain.com/
+```
+####Hexo_config.yml
+change some code to enable archives page
+```
+# Archives
+## 2: Enable pagination
+## 1: Disable pagination
+## 0: Fully Disable
+archive: 1
+category: 1
+tag: 1
+```
+###Icon
+Seclet icon which you like in<code>\TKL\source\css\iconList.css</code>  
+![](http://kieran-hexo.qiniudn.com/hexo_14_1.png)  
+![](http://kieran-hexo.qiniudn.com/hexo_14_2.png)
+###Screen
+Show
+![](http://kieran-hexo.qiniudn.com/hexo_14_3.png)
+  
+Content
+![](http://kieran-hexo.qiniudn.com/hexo_14_4.png)
+
---
+++ source/js/scripts.js
@@ -0,0 +1,189 @@
+$(document).ready(function($) {
+    /**
+     * plugins list
+     * @type {
+     *   jquery:[nicescroll,slidebars,animsition,Bootstrap Auto-Hiding Navbar]
+     *   
+     * }
+     */
+    var a = {
+                info: "既然你都点进控制台想深入看点什么了..那么就送你一个冰淇淋吧，YY感受一下~",
+                logo: "         _.-.  \n" + "       ,'/ //\\ \n" + "      /// // /)\n" + "     /// // //|\n" + "    /// // /// \n" + "   /// // ///  \n" + "  (`: // ///   \n" + "   `;`: ///    \n" + "   / /  `'      \n" + "  / /\n" + " (_/  \n"
+            };
+            window.console && console.info && console.info(a.logo + a.info);
+    
+    // 开始进入时加载位置
+    var scrollstatus = $(document).scrollTop();
+    if(scrollstatus >10){
+       $(".lightnav .navbar-inner").addClass("lightnav-alt");
+    }else{
+       $(".lightnav .navbar-inner").removeClass("lightnav-alt");
+    }
+
+    var window_height = $(window).height();
+    var window_width = $(window).width();
+    $(window).scroll(function () {
+        
+        if ($(document).scrollTop() > 10) {
+            $(".lightnav .navbar-inner").addClass("lightnav-alt");
+        } else {
+            $(".lightnav .navbar-inner").removeClass("lightnav-alt");
+        }
+    });
+
+    var wall_number = "url(http://7bv937.com1.z0.glb.clouddn.com/qcyoung/TKL/wall-"+Math.ceil(Math.random()*45)+".jpg)";
+    // var wall_number = "url(http://images.alphacoders.com/453/45373.jpg";
+    $(".element-img").css('background-image',wall_number);
+
+    // 微信Window
+    $("#navigation .weixin,.social .weixin").bind('click',function(event) {
+      var window_height = $(window).height();
+      var window_width = $(window).width();
+      layer.open({
+        type: 1,
+        title: false,
+        skin: 'layui-layer-demo', //样式类名
+        closeBtn: false, //不显示关闭按钮
+        shift: 2,
+        shadeClose: true, //开启遮罩关闭
+        area: [ window_width , window_height],
+        content: '<img src="http://7bv937.com1.z0.glb.clouddn.com/qcyoung/yangzj1992QRcode.jpg" width="200px" height="200px"/>'
+      });
+    });
+
+    var scrollclick;
+
+    //返回顶部功能
+    $(window).bind("scroll",function() {
+      //获取当前垂直位移值
+      if(!scrollclick){
+        var scrollTopNum=$(document).scrollTop(),
+        //获取浏览器当前高度
+        winHeight=$(window).height(),
+        returnTop=$("div.return-top");
+        //滚动条垂直距离大于0时显示，反之隐藏
+        (scrollTopNum>240)?returnTop.fadeIn("fast"):returnTop.fadeOut('fast');
+      }
+    });
+
+    // 点击按钮后，滚动条的垂直方向的值逐渐变为0，也就是滑动向上的效果
+    $("div.return-top").click(function() {
+      scrollclick = true;
+      $(this).fadeOut('800');
+      $("html, body").animate({ scrollTop: 0 }, 800,function(){
+        scrollclick = false;
+      });
+      return false;
+    });
+
+    // Slidebars off-canvas menu
+    $.slidebars();
+
+    $("html").niceScroll({
+      smoothscroll: true, // scroll with ease movement
+      autohidemode: false,
+      zindex: "100", // change z-index for scrollbar div
+          scrollspeed: 60, // scrolling speed
+          mousescrollstep: 40,// mouse scrolling speed
+          gesturezoom: false,//上缩放框激活时，间距输出/输入
+          horizrailenabled: false,//管理水平滚动
+          cursorcolor: "#151515",
+          boxzoom: false,// enable zoom for box content
+          cursorborder: "0px solid #202020",
+          cursorborderradius: "5px",
+          cursorwidth: 9,
+          enablemousewheel: true,
+          background: "rgba(255,255,255,0.7)",
+    });
+
+    // Page transitions
+    $(".animsition").animsition({
+      inClass               :   'fade-in',
+      outClass              :   'fade-out-down-sm',
+      inDuration            :    500,
+      outDuration           :    500,
+      linkElement           :   '.animsition-link', 
+      //e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+      loading               :    true,
+      loadingParentElement  :   'body', //animsition wrapper element
+      loadingClass          :   'animsition-loading',
+      unSupportCss          : [ 'animation-duration',
+                                '-webkit-animation-duration',
+                                '-o-animation-duration'
+                              ],
+      //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser. 
+      //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+      overlay               :   false,
+      overlayClass          :   'animsition-overlay-slide',
+      overlayParentElement  :   'body'
+    });
+
+    // Functionailty constraints for mobile(wall opacity covering layer)
+    if (!Modernizr.touch) {
+        jQuery(function ($) {
+            // Hero & page-header fade-in effect 
+            var divs = $('.herofade');
+            $(window).on('scroll', function () {
+                var st = $(this).scrollTop();
+                divs.css({
+                    'margin-top': -(st / 0) + "px",
+                    'opacity': 0.7 - st / 1600
+                });
+            });
+        });
+    }
+
+    // autohide navbar on scroll
+    $("div.navbar-fixed-top").autoHidingNavbar({
+      animationDuration: 400,
+      hideOffset: 0,//Hides the navbar after scrolling that much pixel. auto means the navbar's height.
+    });
+
+    // Scrollspy for scrollto links in floating faq sidebar
+    var $body   = $(document.body);
+    var navHeight = $('.navbar').outerHeight(true) + 80;
+
+    $body.scrollspy({
+      target: '#leftcol',
+      offset: navHeight
+    });
+
+      /*!
+       * IE10 viewport hack for Surface/desktop Windows 8 bug
+       * Copyright 2014 Twitter, Inc.
+       * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+       * details, see http://creativecommons.org/licenses/by/3.0/.
+       */
+      // See the Getting Started docs for more information:
+      // http://getbootstrap.com/getting-started/#support-ie10-width
+      if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+          var msViewportStyle = document.createElement('style');
+          msViewportStyle.appendChild(
+              document.createTextNode(
+                  '@-ms-viewport{width:auto!important}'
+              )
+          );
+          document.querySelector('head').appendChild(msViewportStyle);
+      }
+
+      $(".logo").hover(function() {
+        $(this).find("#white-logo").css('display','none');
+        $(this).find("#brown-logo").css('display','block');
+      }, function() {
+        $(this).find("#brown-logo").css('display','none');
+        $(this).find("#white-logo").css('display','block');
+      });
+
+
+      $('.sb-toggle-submenu').off('click') // Stop submenu toggle from closing Slidebars.
+        .on('click', function() {
+          $submenu = $(this).parent().children('.sb-submenu');
+          $(this).add($submenu).toggleClass('sb-submenu-active'); // Toggle active class.
+    
+          if ($submenu.hasClass('sb-submenu-active')) {
+          $submenu.slideDown(200);
+          } else {
+          $submenu.slideUp(200);
+        }
+      });
+}(jQuery)); // End "use strict"
